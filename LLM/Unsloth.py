413d7fa5eb4a8366db4d3366ecb27e1b350304da
Leonardo Piano <leopiano.98@gmail.com>
2024-12-10 13:44:16 +0100

    Update unsloth.py

--- LLM/Unsloth.py
+++ LLM/Unsloth.py
@@ -5,24 +5,13 @@ from unsloth.chat_templates import get_chat_template
 class UnslothLLM():
     def __init__(self, model_id,max_seq_length=2048,inference=True):
         model, tokenizer = FastLanguageModel.from_pretrained(
-            model_name=model_id,  # YOUR MODEL YOU USED FOR TRAINING
+            model_name=model_id,
             max_seq_length=max_seq_length,
             dtype=None,
             load_in_4bit=True,
             device_map="cuda:0"
         )
-        """
-        template=""
-        if "llama" in model_id or "anita" in model_id:
-            template="llama-3"
-        else:
-            template="phi-3"
-        
-        tokenizer = get_chat_template(
-            tokenizer,
-            chat_template=template,
-            mapping={"role": "from", "content": "value", "user": "human", "assistant": "gpt"},
-        )"""
+
         
         if inference:
             FastLanguageModel.for_inference(model)
@@ -30,22 +19,20 @@ class UnslothLLM():
         self.model_id = model_id
         self.tokenizer = tokenizer
         self.model = model
-        #self.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
-        #self.tokenizer.padding_side = 'right'
+
     def prepare_peft_model(self):
         return FastLanguageModel.get_peft_model(
             self.model,
-            r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+            r=16,
             target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
                             "gate_proj", "up_proj", "down_proj", ],
             lora_alpha=16,
-            lora_dropout=0,  # Supports any, but = 0 is optimized
-            bias="none",  # Supports any, but = "none" is optimized
-            # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
-            use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+            lora_dropout=0,
+            bias="none",
+            use_gradient_checkpointing="unsloth",
             random_state=3407,
-            use_rslora=False,  # We support rank stabilized LoRA
-            loftq_config=None,  # And LoftQ
+            use_rslora=False,
+            loftq_config=None,
         )
     def generate(self,messages,max_new_tokens=512):
         inputs = self.tokenizer.apply_chat_template(
@@ -56,7 +43,8 @@ class UnslothLLM():
         ).to("cuda")
 
         outputs = self.model.generate(input_ids=inputs,pad_token_id=self.tokenizer.pad_token_id,max_new_tokens=max_new_tokens, temperature=0.0,    do_sample=False,use_cache=True)
-        answer = self.tokenizer.batch_decode(outputs,skip_special_tokens=True)[0]
+        response = outputs[0][inputs.shape[-1]:]
+        answer = self.tokenizer.decode(response, skip_special_tokens=True)
         return answer
 
     def formatting_prompts_func(self,examples):
